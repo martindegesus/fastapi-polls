f4af6767bf71cb8ae86b853390bca2921e97d33e
martin de gesus <martindg_72@hotmail.com>
2021-10-11 23:35:12 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    email: str
+
+class Poll(BaseModel):
+    title: str
+    type: str
+    is_add_choices_active: bool
+    is_voting_active: bool
+    created_by: User
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/polls")
+async def root():
+    return {"polls": "Hello World"}
+
+@app.get("/users")
+async def root():
+    return {"users": "Hello World"}
+
+@app.post("/users/")
+async def create_user(user: User):
+    return user
+
+@app.post("/polls/")
+async def create_poll(poll: Poll):
+    return poll
